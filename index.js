63b60221e9bc5a0db4ba0e7821995a8f80611f1f
erin <erinverkoulen93@gmail.com>
2021-06-03 13:58:30 -0500

    commit tree b2cab99c00deb552c67a51865a3a3b8c613a8e69

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+function submitData(userName, email) {
+    const formData = {
+        name: userName,
+        email: email
+    }
+    const configObj = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    };
+
+    const fetchData = fetch('http://localhost:3000/users', configObj)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        const li = document.createElement('li');
+          
+          li.textContent = object.id;
+          console.log(object.id);
+          document.body.appendChild(li);
+    })
+    .catch(function(message) {
+        const p = document.createElement('p');
+        p.textContent = message;
+        document.body.appendChild(p);
+    })
+    return fetchData;
+
+}
